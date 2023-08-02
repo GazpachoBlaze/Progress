d872bbf635a7fe1790c9a1c949157d7362458373
Gazpacho <Gazpachoblaze@gmail.com>
2023-08-02 16:26:26 -0500

    angular routing for panel page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './routing/app.routing.module';
-
+import { AppRoutingModule, routingComponents } from './routing/app.routing.module';
 
 import { AppComponent } from './app.component';
 
-import { HomeComponent } from './components/home/home.component';
-import { PricepanelComponent } from './components/pricepanel/pricepanel.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PricepanelComponent,
-  ],
+  declarations: [AppComponent, routingComponents],
 
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
 
   providers: [],
 
-  bootstrap: [AppComponent]
-
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+
+
+export class AppModule {}
--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-
 import { HomeComponent } from '../components/home/home.component';
 import { PricepanelComponent } from '../components/pricepanel/pricepanel.component';
 
 const routes: Routes = [
-    {path: "home", component: HomeComponent},
-    {path: "pricepanel", component: PricepanelComponent}
-  ];
+  { path: 'home', component: HomeComponent },
+  { path: 'pricepanel', component: PricepanelComponent },
+];
 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AppRoutingModule {}
 
-
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: []
-  })
-
-  export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, PricepanelComponent];
