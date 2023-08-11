87e460fe07902855c93cea5cbfcf4babb29691d1
Gazpacho <Gazpachoblaze@gmail.com>
2023-08-11 15:51:17 -0500

    add grid list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './routing/app.routing.module';
 
 import { AppComponent } from './app.component';
+import { GridSystemComponent } from './components/grid-system/grid-system.component';
 
 
 
 
 @NgModule({
-  declarations: [AppComponent, routingComponents,],
+  declarations: [AppComponent, routingComponents, GridSystemComponent,],
 
   imports: [BrowserModule, AppRoutingModule],
 
